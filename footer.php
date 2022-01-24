eb6cf9d65fb3409385c83d02132026a1795684dc
Vanno97 <m.vanini97@yahoo.com>
2022-01-24 23:52:02 +0100

    New Project

--- footer.php
+++
@@ -1,4 +0,0 @@
-        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-        <script src="assets/js/main.js"></script>
-    </body>
-</html>
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "controller/VotazioneController.php";
-$dao = new VotazioneController();
-echo "<pre>";
-var_dump($dao->getRisultati());
-echo "</pre>";
